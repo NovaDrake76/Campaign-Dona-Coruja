2e6ca407808e08e4e703aefbc533a9c1e69fc970
NovaDrake76 <novadrake76@gmail.com>
2023-12-04 11:06:38 -0300

    feat: metatags

--- src/pages/Donation/Donation.tsx
+++ src/pages/Donation/Donation.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import Button from "../../components/Button";
 import { FaCopy, FaCheck } from "react-icons/fa";
+import { FadeLoader } from "react-spinners";
 
 const Donation = () => {
     const [copied, setCopied] = useState(false)
     const [copiedNumber, setCopiedNumber] = useState(false)
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    }
 
     const copyQRCode = () => {
         navigator.clipboard.writeText("00020126730014BR.GOV.BCB.PIX0136fcace115-0650-48bc-876a-9c54d72a9c040211Dona Coruja5204000053039865802BR5923Eugenia Kelly de Araujo6009SAO PAULO62140510ZVeWxsXY6u6304AC56")
@@ -29,8 +35,11 @@ const Donation = () => {
     return (
         <div className="bg-[url('./assets/bg-1.webp')] bg-repeat-none bg-cover ">
             <div className="h-screen w-screen bg-[url('./assets/donation-bg.webp')] bg-repeat-none bg-cover flex flex-col md:flex-row items-center justify-center">
-                <div className="p-4 py-8 bg-white rounded-3xl  flex flex-col items-center gap-4 z-10">
-                    <img src="/images/qrcode.png" alt="qr code for donation" width={184} height={184} />
+                <div className="p-4 py-8 bg-white rounded-3xl  flex flex-col items-center gap-4 z-10 w-[216px] h-[302px]">
+                    {isLoading && <div className="flex justify-center items-center mt-4 ml-2">
+                        <FadeLoader color="#ee59b3" />
+                    </div>}
+                    <img src="/images/qrcode.png" alt="qr code for donation" width={184} height={184} onLoad={handleImageLoad} />
                     <Button text={
                         copied ? "Código copiado!" : "Copiar Código"
                     } icon={<FaCopy />} size="s" onClick={copyQRCode} />

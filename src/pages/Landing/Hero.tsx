d73e862769527e4b055b153bdfe629204bebd9fe
NovaDrake76 <novadrake76@gmail.com>
2023-12-04 14:44:09 -0300

    fix: Hero text gap

--- src/pages/Landing/Hero.tsx
+++ src/pages/Landing/Hero.tsx
@@ -17,7 +17,9 @@ const Hero = () => {
         >
             <div className="flex flex-col items-center p-2 md:p-0 text-center text-white">
                 <h1 className="font-[300] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Com a sua ajuda, centenas de crianças</h1>
-                <div className="flex items-center font-[300] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl gap-4">conquistarão <h2 className="font-[700]">um futuro melhor</h2></div>
+                <div className="flex items-center font-[300] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl gap-2 md:gap-4">conquistarão
+                    <h2 className="font-[700]">um futuro melhor</h2>
+                </div>
             </div>
             <Link to="/donation">
                 <Button text="Faça agora sua doação" size="m" color="primary" />

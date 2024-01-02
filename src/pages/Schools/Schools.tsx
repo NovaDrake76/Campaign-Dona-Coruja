04fd2efd1fb1afd3f19d1e86f3629e3db0ab3238
Nathan Marques <nathan.marques176@gmail.com>
2024-01-02 11:13:06 -0300

    feat: register school link

--- src/pages/Schools/Schools.tsx
+++ src/pages/Schools/Schools.tsx
@@ -2,6 +2,11 @@ import { t } from "i18next";
 import Button from "../../components/Button";
 
 const Schools = () => {
+
+    const handleButtonClick = () => {
+        window.open("https://docs.google.com/forms/d/e/1FAIpQLSc7cTYadP5-PK41Mwbfg0Xt6VXK0SQZZZ3QFGdwMPL87NilLw/viewform?usp=sf_link", "_blank", "noopener,noreferrer");
+    };
+
     return (
         <div className="bg-[url('./assets/bg-1.webp')] bg-repeat-none bg-cover ">
             <div className="h-screen w-screen bg-[url('./assets/donation-bg.webp')] bg-repeat-none bg-cover flex flex-col md:flex-row items-center justify-center p-8 md:p-0 ">
@@ -9,7 +14,7 @@ const Schools = () => {
                     {
                         t("schools.mainText")
                     }
-                    <div> <Button text={t("schools.button")} /></div>
+                    <div> <Button text={t("schools.button")} onClick={handleButtonClick} /></div>
 
                 </div>
             </div>
